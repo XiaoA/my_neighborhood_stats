1d471014a395776f3b6aebce223074a009fa87e6
XiaoA <abuckingham99@gmail.com>
2016-02-07 19:52:17 -0800

    Move invalid signup spec logic to feature file

--- spec/features/creating_user_spec.rb
+++ spec/features/creating_user_spec.rb
@@ -8,17 +8,7 @@ RSpec.feature "Creating users" do
   end
 
     scenario "with invalid credentials" do
-    visit root_path
-
-    click_link 'Sign Up'
-    fill_in 'First name', with: nil
-    fill_in 'Last name', with: nil
-    fill_in 'Email', with: 'whatever'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-
-    click_button 'Create User'
-
-    expect(page).to have_content('User has not been created.')
+      create_user_invalid_credentials
+      expect(page).to have_content('User has not been created.')
   end
 end
