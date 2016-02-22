0670055455ad36c0726b0fcad3177725be76dcec
XiaoA <abuckingham99@gmail.com>
2016-02-21 16:52:03 -0800

    Update specs from 'Sign in' to 'Sign In'

--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -4,7 +4,7 @@ module Features
     click_on "Sign in"
     fill_in "Email", with: "sherlock@example.com"
     fill_in "Password", with: "e1m3taRY!#MyD3arWa2*"
-    click_on "Sign in"
+    click_on "Sign In"
   end
 
     def sign_in_with_invalid_credentials
@@ -12,7 +12,7 @@ module Features
     click_on "Sign in"
     fill_in "Email", with: nil
     fill_in "Password", with: "e1m3t!#MyD3arWa2*"
-    click_on "Sign in"
+    click_on "Sign In"
   end
 
   def create_user_valid_credentials
